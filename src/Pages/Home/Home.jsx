6e51058e09baf51c31edf883160cc7fdbb5976f9
Shahil Chettri <sahilchettri8848@gmail.com>
2024-06-10 20:14:28 +0545

    Updated Home component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,12 +5,12 @@ import Flow from "../../Components/Flow";
 import discountbus from "../../Photos/discountbus.png";
 import Services from "../../Components/Services";
 
-const Home = ({ userPageName }) => {
+const Home = () => {
   return (
     <Layout>
       <div className="main-container">
         <div className="main-text">
-          <h1>RIMJIM Travels {userPageName}</h1>
+          <h1>RIMJIME Travels </h1>
           <h6>Make your travel along with the patiece with travel</h6>
           <button className="main-button">Explore more</button>
         </div>
